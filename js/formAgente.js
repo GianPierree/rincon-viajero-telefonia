57fb4174c64f708b031912851d264d6a2b2ccf52
Gian Munoz <gian0291@gmail.com>
2023-10-18 18:21:34 -0500

    feat: Se refactorizo el código

--- js/formAgente.js
+++ js/formAgente.js
@@ -34,7 +34,7 @@ $.ajax(statusLead).done(function (response) {
     for (var key in obj) {
         htmlEstadoLead += "<option value=" + obj[key]["STATUS_ID"] + ">" + obj[key]["NAME"] + "</option>";
     }
-    document.getElementById("campanaEstado").innerHTML = htmlEstadoLead;
+    document.getElementById("campanaEstadoLead").innerHTML = htmlEstadoLead;
 });
 
 var source = {
