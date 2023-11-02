41c9f22d2a5d4838888d5a8339a214273a19a8c2
Gian Munoz <gian0291@gmail.com>
2023-11-02 02:19:08 -0500

    feat: Se agrego el nuevo query

--- update-lead.php
+++ update-lead.php
@@ -1,6 +1,33 @@
 <?php
+require("./database/conn.php");
+require("./functions/Log.php");
+
+$log = new Log;
+
 // print_r($_REQUEST);
-writeToLog($_REQUEST, 'incoming');
+
+$arrId = $_REQUEST["document_id"][2];
+$id = explode("_", $arrId);
+writeToLog($id, 'incoming');
+$idLead = $id[1];
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo json_encode(array('success' => "Error de conexión: " . $conn->connect_error));
+} else {
+    $sqlD = "DELETE FROM leads WHERE leadId = $idLead";
+    $log->logRequest($sqlD, "Log SQL");
+
+    if ($conn->query($sqlD) === TRUE) {
+        $r = "Lead id eliminado " . $idLead;
+        echo json_encode(array('success' => "Eliminación de lead."));
+    } else {
+        $r = "Error deleting record $idLead : " . $conn->error;
+        echo json_encode(array('success' => "Error: " . $sql . "<br>" . $conn->error));
+    }
+    
+    $conn->close();
+}
 
 /**
  * Write data to log file.
